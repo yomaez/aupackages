fdc9dde212ab0f51401eda9ac1031943dcede436
yomaez <tmahony34@gmail.com>
2021-11-15 09:47:42 +1100

    Update vscode
    Update vscode

---
+++ au-packages/visualstudiocode/tools/chocolateyinstall.ps1
@@ -0,0 +1,33 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' #only one of these: exe, msi, msu
+  url64bit      = 'https://update.code.visualstudio.com/1.62.2/win32-x64/stable'
+  #file         = $fileLocation
+  softwareName  = 'visualstudiocode*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum64    = ''
+  checksumType64= 'sha256' #default is checksumType
+
+  # MSIs
+  # OTHERS
+  # Uncomment matching EXE type (sorted by most to least common)
+  silentArgs   = '/S'           # NSIS
+  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  #silentArgs   = '/s'           # InstallShield
+  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
+  #silentArgs   = '/s'           # Wise InstallMaster
+  #silentArgs   = '-s'           # Squirrel
+  #silentArgs   = '-q'           # Install4j
+  #silentArgs   = '-s'           # Ghost
+  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
+  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
+                                 #       https://community.chocolatey.org/packages/autohotkey.portable
+  validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Install-ChocolateyPackage @packageArgs 
---
+++ au-packages/visualstudiocode/update.ps1
@@ -0,0 +1,33 @@
+﻿import-module au
+
+
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    
+
+
+    $AppPackage = "vscode" #Choco Package Name
+
+    #Pull Version from Choco List
+    $PackageVersion = choco list $AppPackage -e | Select -Index 1 
+    $CharArray =$PackageVersion.Split(" ")
+    $LatestPackageVersion = $CharArray[1]
+
+    
+    
+    @{
+        URL64   = 'https://update.code.visualstudio.com/' + $LatestPackageVersion + '/win32-x64/stable'
+        Version = $LatestPackageVersion
+    }
+}
+
+update -ChecksumFor 64
